61af53cfeadb518169e7709b54e04095be231301
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-17 14:17:27 +0200

    Update variables.js

--- Variables/variables.js
+++ Variables/variables.js
@@ -10,3 +10,20 @@ console.log("Is Mbali the greatest coder?:", greatest_coder)
 document.getElementById("p1").innerHTML = "Hello " + firstname + "Welcome my sweet lemon"
 document.getElementById("p2").innerHTML = "you are" + age + "years old"
 document.getElementById("p3").innerHTML = "Is Mbali the greatest coder?:" +  greatest_coder
+
+
+
+// const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+//PI = 420.69;
+
+circumference = 2 * PI * radius;
+
+console.log("The circumference is:", circumference)
