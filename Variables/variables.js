d6846d23e17071b5f890b267f7da30ae88f09c61
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-17 12:23:02 +0200

    Update variables.js

--- Variables/variables.js
+++ Variables/variables.js
@@ -1,1 +1,12 @@
+let firstname = "Mbali Phulwane"
+let age = 27
+let greatest_coder = true
 
+console.log("hello", firstname,"welcome my sweet lemon" );
+console.log("you are:", age, "years old");
+console.log("Is Mbali the greatest coder?:", greatest_coder)
+
+
+document.getElementById("p1").innerHTML = "Hello " + firstname + "Welcome my sweet lemon"
+document.getElementById("p2").innerHTML = "you are" + age + "years old"
+document.getElementById("p3").innerHTML = "Is Mbali the greatest coder?:" +  greatest_coder
