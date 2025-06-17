57baa3ceafbfcf53e5b2bb4046a89e92c481313e
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-17 13:40:29 +0200

    Update user_input.js

--- use_input/user_input.js
+++ use_input/user_input.js
@@ -1,4 +1,14 @@
-let username = window.prompt("what is your name?");
-console.log(username);
+// let username = window.prompt("what is your name?");
+// console.log(username);
+
+let username;
+
+document.getElementById("myButton").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello" + username;
+
+}
 
 
