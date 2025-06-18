477596d153c25420d4d22b3d19ec144da57ff96b
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-18 11:12:30 +0200

    Update Stringmethods.js

--- String_Methods/Stringmethods.js
+++ String_Methods/Stringmethods.js
@@ -36,3 +36,33 @@ console.log('Does userName include "Bro"?', userName.includes("Bro")); // Output
 // Additional Example: Split the phoneNumber into an array of digits
 const phoneDigits = phoneNumber.split("");
 console.log("Phone number digits:", phoneDigits); // Outputs: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+// slice() extracts a section of a string 
+//             and returns it as a new string, 
+//             without modifying the original string
+
+let fullName = "Snoop Dogg";
+let firstName;
+let lastName;
+
+//firstName = fullName.slice(0, 3);
+//lastName = fullName.slice(4);
+
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+console.log(firstName);
+console.log(lastName);
+
+
+// method chaining = calling one method after another
+//                   in one continuous line of code
+
+let userName = "bro";
+
+let letter = userName.charAt(0).toUpperCase().trim();
+
+console.log(letter)
+
+
+
