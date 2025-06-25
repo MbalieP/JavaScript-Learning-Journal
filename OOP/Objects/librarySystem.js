d35380afeebf48cac1eb2f463d08b51a5850b0ed
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-25 06:13:07 +0200

    Update librarySystem.js

--- OOP/Objects/librarySystem.js
+++ OOP/Objects/librarySystem.js
@@ -7,5 +7,17 @@ const library = {
         { title: "To Kill a Mockingbird", author: "Harper Lee", isAvailable: true },
         { title: "The Great Gatsby", author: "F. Scott Fitzgerald", isAvailable: false }
     ],
+      // Method to display all available books
+    showAvailableBooks: function () {
+        console.log(`📚 Available books in ${this.name}:`);
+        this.books.forEach(book => {
+            if (book.isAvailable) {
+                console.log(`- ${book.title} by ${book.author}`);
+            }
+        });
+    },
+
+    
+    
 
   
