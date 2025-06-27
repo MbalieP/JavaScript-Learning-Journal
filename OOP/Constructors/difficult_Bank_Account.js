357a71436e50a1e4311aa4246271f5e19c4e5450
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-27 06:17:35 +0200

    Update difficult_Bank_Account.js

--- OOP/Constructors/difficult_Bank_Account.js
+++ OOP/Constructors/difficult_Bank_Account.js
@@ -1,1 +1,12 @@
+class BankAccount {
+    static bankName = "Mzansi National Bank"; // Shared by all accounts
 
+    constructor(owner, accountType = "Cheque", balance = 0) {
+        this.owner = owner;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.transactions = []; // Store all transactions
+        this.accountNumber = BankAccount.generateAccountNumber();
+    }
+
+ 
