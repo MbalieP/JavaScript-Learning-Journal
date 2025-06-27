f2478e41456a62092ac866d9ac71210ee5966b21
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-27 06:18:07 +0200

    Update difficult_Bank_Account.js

--- OOP/Constructors/difficult_Bank_Account.js
+++ OOP/Constructors/difficult_Bank_Account.js
@@ -8,5 +8,28 @@ class BankAccount {
         this.transactions = []; // Store all transactions
         this.accountNumber = BankAccount.generateAccountNumber();
     }
+   // 📈 Deposit method with validation
+    deposit(amount) {
+        if (amount <= 0) {
+            console.log("❌ Deposit must be more than R0");
+            return;
+        }
+        this.balance += amount;
+        this.transactions.push({
+            type: "Deposit",
+            amount: amount,
+            date: new Date()
+        });
+        console.log(`✅ ${this.owner} deposited ${this.formatRands(amount)}. New balance: ${this.formatRands(this.balance)}`);
+    }
+
+  
+
+console.log("\n---\n");
+
+acc2.deposit(500);
+acc2.withdraw(300);
+acc2.getBalance();
+acc2.showTransactions();
 
  
