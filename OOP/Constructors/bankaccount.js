3946ffb4975ec701e91a58468af7c748dd1d0d10
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-27 06:08:40 +0200

    Update bankaccount.js

--- OOP/Constructors/bankaccount.js
+++ OOP/Constructors/bankaccount.js
@@ -2,3 +2,13 @@
 function BankAccount(owner, balance = 0) {
     this.owner = owner;         //  Account holder's name
     this.balance = balance;     // Initial balance in ZAR
+
+    //  Method to deposit money
+    this.deposit = function(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            console.log(`${this.owner} deposited R${amount}. New balance: R${this.balance}`);
+        } else {
+            console.log(" Deposit amount must be positive.");
+        }
+    };
